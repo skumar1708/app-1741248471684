614fbe879367aedaa51b6e9a04145bae726e7a39
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-06 13:38:49 +0530

    Automated commit: frontend/src/pages/GamePage.js

---
+++ frontend/src/pages/GamePage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import SnackBoard from '../components/SnackBoard';
+
+function GamePage() {
+  return (
+    <div>
+      <Header />
+      <SnackBoard />
+      <Footer />
+    </div>
+  );
+}
+
+export default GamePage;
